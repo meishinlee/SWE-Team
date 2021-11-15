6e1128c25724a54d274c2e2b4bd525dbe60118a9
Mei Shin Lee <53961253+meishinlee@users.noreply.github.com>
2021-11-15 15:47:46 -0500

    pushed common.mk file

---
+++ common.mk
@@ -0,0 +1,23 @@
+LINTER = flake8
+API_DIR = API
+DB_DIR = db
+REQ_DIR = .
+PYDOC = python3 -m pydoc -w
+TESTFINDER = nose2
+
+export TEST_MODE = 1
+export DEMO_HOME = "C:\Users\miche\Desktop\Fall2021\SWE-Team"
+
+FORCE:
+
+tests: lint unit
+
+unit: FORCE
+	$(TESTFINDER) --with-coverage
+
+lint: FORCE
+	$(LINTER) *.py
+
+docs: FORCE
+	$(PYDOC) ./*.py
+	git add ./*.html
