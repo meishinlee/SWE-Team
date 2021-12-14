5c93e39962b7f8e628ced66f94ad786b4d41c859
Aaron Chen <aaron.chen11219@gmail.com>
2021-12-14 11:36:45 -0500

    Following Callahan's tutorial. He removed content from mongo_port.py and we are using his current stuff

--- db/mongo_port.py
+++ db/mongo_port.py
@@ -13,56 +13,9 @@ It assumes that cause that's what we've been using!
 """
 import sys
 import json
+import pymongo as pm 
 
-import db_connect as dbc
-
-
-def read_collection(json_version):
-    """
-    A function to read a colleciton off of disk.
-    """
-    try:
-        with open(json_version) as file:
-            return json.loads(file.read())
-    except FileNotFoundError:
-        print(f"{json_version} not found.")
-        return None
-
-
-def new_ent_from_json(key_name, ent_name, ent_data):
-    dict1 = {key_name: ent_name}
-    return {**dict1, **ent_data}
-
-
-client = dbc.get_client()
+client = pm.MongoClient()
 print(client)
 
-if len(sys.argv) < 4:
-    # the key in the JSON file will become an ordinary field
-    # in the Mongo DB, but we need to give it a name!
-    print(f"Usage: {sys.argv[0]} db_name collection_name key_name")
-    exit(1)
 
-db = client[sys.argv[1]]
-print(db)
-
-collect_nm = sys.argv[2]
-print(f"{collect_nm=}")
-collection = db[collect_nm]
-
-json_file = collect_nm + ".json"
-print(f"{json_file=}")
-
-key_name = sys.argv[3]
-print(f"{key_name=}")
-
-collect = read_collection(json_file)
-
-for entity_nm in collect:
-    new_entity = new_ent_from_json(key_name,
-                                   entity_nm,
-                                   collect[entity_nm])
-    print(f"{new_entity=}")
-    collection.insert_one(new_entity)
-
-print(collection)
